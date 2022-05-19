94f68e5d2cff316d5e12cea8aeb244c151851034
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2022-05-19 15:40:27 +0100

    Plot statistical model results

--- gp_cross_validation.py
+++ gp_cross_validation.py
@@ -16,38 +16,64 @@ y = training_data[:,3]
 
 #load the wake model prior mean
 ctstar_wake_model = np.genfromtxt('ctstar_wake_model.csv', delimiter=',')
-prior_mean = ctstar_wake_model[:,2]
 
-#array to hold results
-error = np.zeros(50)
+#array to hold statistical model results
+ctstar_statistical_model = np.zeros((50,4))
 
-loo = LeaveOneOut()
-for train_index, test_index in loo.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    print(test_index)
+for i in range(4):
+  loo = LeaveOneOut()
+  for train_index, test_index in loo.split(X):
+      X_train, X_test = X[train_index], X[test_index]
+      y_train, y_test = y[train_index], y[test_index]
 
-    #standardise the feature set of the training and test data
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train_stan = scaler.transform(X_train)
-
-
-    #create kernel for Gaussian Process Regression
-    kernel = 1.0 ** 2 * RBF(length_scale=[1.,1.,1.]) + WhiteKernel(noise_level=1e-3, noise_level_bounds=[1e-10,1])
-    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=50)
+      #standardise the feature set of the training and test data
+      scaler = StandardScaler()
+      scaler.fit(X_train)
+      X_train_stan = scaler.transform(X_train)
 
 
-    #fit GP and make predictions
-    gp.fit(X_train_stan,y_train-prior_mean[train_index])
-    print(gp.kernel_)
+      #create kernel for Gaussian Process Regression
+      kernel = 1.0 ** 2 * RBF(length_scale=[1.,1.,1.]) + WhiteKernel(noise_level=1e-3, noise_level_bounds=[1e-10,1])
+      gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=50)
 
-    X_test_stan = scaler.transform(X_test)
 
-    print(mean_absolute_error(y_test, gp.predict(X_test_stan) + 
-      prior_mean[test_index])/0.75)
-    error[test_index] = mean_absolute_error(y_test, gp.predict(X_test_stan) + 
-      prior_mean[test_index])/0.75
-    
-print(np.mean(error))
-print(np.max(error))
+      #fit GP and make predictions
+      gp.fit(X_train_stan,y_train-ctstar_wake_model[train_index,i])
+
+      X_test_stan = scaler.transform(X_test)
+
+      ctstar_statistical_model[test_index,i] = gp.predict(X_test_stan) + ctstar_wake_model[test_index,i]
+ 
+  print(mean_absolute_error(ctstar_statistical_model[:,i], training_data[:,3])/0.75)
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[5.33,5.33])
+ax[0,0].scatter(training_data[:,3], ctstar_wake_model[:,0], label = 'Wake model')
+ax[0,0].scatter(training_data[:,3], ctstar_statistical_model[:,0], label = 'Statistical model')
+ax[0,0].plot([0.55,0.8],[0.55,0.8], c='k')
+ax[0,0].set_xlim([0.55,0.8])
+ax[0,0].set_ylim([0.45,0.8])
+ax[0,0].legend()
+
+ax[0,1].scatter(training_data[:,3], ctstar_wake_model[:,1], label = 'Wake model')
+ax[0,1].scatter(training_data[:,3], ctstar_statistical_model[:,1], label = 'Statistical model')
+ax[0,1].plot([0.55,0.8],[0.55,0.8], c='k')
+ax[0,1].set_xlim([0.55,0.8])
+ax[0,1].set_ylim([0.45,0.8])
+ax[0,1].legend()
+
+ax[1,0].scatter(training_data[:,3], ctstar_wake_model[:,2], label = 'Wake model')
+ax[1,0].scatter(training_data[:,3], ctstar_statistical_model[:,2], label = 'Statistical model')
+ax[1,0].plot([0.55,0.8],[0.55,0.8], c='k')
+ax[1,0].set_xlim([0.55,0.8])
+ax[1,0].set_ylim([0.45,0.8])
+ax[1,0].legend()
+
+ax[1,1].scatter(training_data[:,3], ctstar_wake_model[:,3], label = 'Wake model')
+ax[1,1].scatter(training_data[:,3], ctstar_statistical_model[:,3], label = 'Statistical model')
+ax[1,1].plot([0.55,0.8],[0.55,0.8], c='k')
+ax[1,1].set_xlim([0.55,0.8])
+ax[1,1].set_ylim([0.45,0.8])
+ax[1,1].legend()
+
+plt.tight_layout()
+plt.savefig('statistical_model_results.png')
