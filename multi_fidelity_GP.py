081fdafb7921f815fa65d65136997da27ed0fedb
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2022-07-07 14:54:08 +0100

    Add LOOCV for multi-fidelity model

--- multi_fidelity_GP.py
+++ multi_fidelity_GP.py
@@ -3,81 +3,65 @@ import matplotlib.pyplot as plt
 
 import GPy
 import emukit.multi_fidelity
-import emukit.test_functions
 from emukit.model_wrappers.gpy_model_wrappers import GPyMultiOutputWrapper
 from emukit.multi_fidelity.models import GPyLinearMultiFidelityModel
+from sklearn.preprocessing import StandardScaler
 
-high_fidelity = emukit.test_functions.forrester.forrester
-low_fidelity = emukit.test_functions.forrester.forrester_low
+#load the LES training data
+training_data = np.genfromtxt('training_data.csv', delimiter = ',')
+X = training_data[:,:3]
+y = training_data[:,3]
 
-x_plot = np.linspace(0, 1, 200)[:, None]
-y_plot_l = low_fidelity(x_plot)
-y_plot_h = high_fidelity(x_plot)
-
-x_train_l = np.atleast_2d(np.random.rand(12)).T
-x_train_h = np.atleast_2d(np.random.permutation(x_train_l)[:6])
-y_train_l = low_fidelity(x_train_l)
-y_train_h = high_fidelity(x_train_h)
+#load the wake model prior mean
+ctstar_wake_model = np.genfromtxt('ctstar_wake_model.csv', delimiter=',')
+ctstar_statistical_model = np.zeros(50)
 
 from emukit.multi_fidelity.convert_lists_to_array import convert_x_list_to_array, convert_xy_lists_to_arrays
 
-X_train, Y_train = convert_xy_lists_to_arrays([x_train_l, x_train_h], [y_train_l, y_train_h])
+for i in range(50):
 
-## Construct a linear multi-fidelity model
+    #create training and testing data
+    train_index = list(filter(lambda x: x!= i, range(50)))
+    test_index = i
 
-kernels = [GPy.kern.RBF(1), GPy.kern.RBF(1)]
-lin_mf_kernel = emukit.multi_fidelity.kernels.LinearMultiFidelityKernel(kernels)
-gpy_lin_mf_model = GPyLinearMultiFidelityModel(X_train, Y_train, lin_mf_kernel, n_fidelities=2)
-gpy_lin_mf_model.mixed_noise.Gaussian_noise.fix(0)
-gpy_lin_mf_model.mixed_noise.Gaussian_noise_1.fix(0)
+    X_train = X[train_index,:]
+    X_test = X[test_index,:][None,:]
+    y_train = y[train_index][:,None]
+    y_test = y[test_index]
 
-lin_mf_model = model = GPyMultiOutputWrapper(gpy_lin_mf_model, 2, n_optimization_restarts=5)
+    #scale the data
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X_train_stan_l = scaler.transform(X)
+    X_train_stan_h = scaler.transform(X_train)
+    X_train_mf, Y_train_mf = convert_xy_lists_to_arrays([X_train_stan_l, X_train_stan_h], [ctstar_wake_model[:,2][:,None], y[train_index,None] ])
 
-## Fit the model
-  
-lin_mf_model.optimize()
+    ## Construct a linear multi-fidelity model
+    kernel_lofi = GPy.kern.RBF(input_dim=3,ARD=True) + GPy.kern.White(input_dim=3)
+    kernel_hifi = GPy.kern.RBF(input_dim=3,ARD=True) + GPy.kern.White(input_dim=3)
 
-## Convert x_plot to its ndarray representation
+    kernels = [kernel_lofi, kernel_hifi]
+    lin_mf_kernel = emukit.multi_fidelity.kernels.LinearMultiFidelityKernel(kernels)
+    gpy_lin_mf_model = GPyLinearMultiFidelityModel(X_train_mf, Y_train_mf, lin_mf_kernel, n_fidelities=2)
+    gpy_lin_mf_model.mixed_noise.Gaussian_noise.fix(0)
+    #gpy_lin_mf_model.mixed_noise.Gaussian_noise_1.fix(0)
 
-X_plot = convert_x_list_to_array([x_plot, x_plot])
-X_plot_l = X_plot[:len(x_plot)]
-X_plot_h = X_plot[len(x_plot):]
+    lin_mf_model = model = GPyMultiOutputWrapper(gpy_lin_mf_model, 2, n_optimization_restarts=5)
 
-## Compute mean predictions and associated variance
+    ## Fit the model
+    lin_mf_model.optimize()
 
-lf_mean_lin_mf_model, lf_var_lin_mf_model = lin_mf_model.predict(X_plot_l)
-lf_std_lin_mf_model = np.sqrt(lf_var_lin_mf_model)
-hf_mean_lin_mf_model, hf_var_lin_mf_model = lin_mf_model.predict(X_plot_h)
-hf_std_lin_mf_model = np.sqrt(hf_var_lin_mf_model)
+    #create test points
+    X_test_stan = scaler.transform(X_test)
+    X_test_mf = convert_x_list_to_array([X_test_stan, X_test_stan])
+    X_test_l = X_test_mf[0,:][None,:]
+    X_test_h = X_test_mf[1,:][None,:]
 
-## Create standard GP model using only high-fidelity data
+    #make predictions
+    lf_mean_mf_model, lf_var_mf_model = lin_mf_model.predict(X_test_l)
+    hf_mean_mf_model, hf_var__mf_model = lin_mf_model.predict(X_test_h)
+    ctstar_statistical_model[test_index] = hf_mean_mf_model
+    print(lf_mean_mf_model, ctstar_wake_model[test_index,2], hf_mean_mf_model, training_data[test_index,3])
 
-kernel = GPy.kern.RBF(1)
-high_gp_model = GPy.models.GPRegression(x_train_h, y_train_h, kernel)
-high_gp_model.Gaussian_noise.fix(0)
-
-## Fit the GP model
-
-high_gp_model.optimize_restarts(5)
-
-## Compute mean predictions and associated variance
-
-hf_mean_high_gp_model, hf_var_high_gp_model  = high_gp_model.predict(x_plot)
-hf_std_hf_gp_model = np.sqrt(hf_var_high_gp_model)
-
-plt.figure(figsize=(12, 8))
-
-plt.fill_between(x_plot.flatten(), (hf_mean_lin_mf_model - 1.96*hf_std_lin_mf_model).flatten(), 
-                 (hf_mean_lin_mf_model + 1.96*hf_std_lin_mf_model).flatten(), facecolor='y', alpha=0.3)
-plt.fill_between(x_plot.flatten(), (hf_mean_high_gp_model - 1.96*hf_std_hf_gp_model).flatten(), 
-                 (hf_mean_high_gp_model + 1.96*hf_std_hf_gp_model).flatten(), facecolor='k', alpha=0.1)
-
-plt.plot(x_plot, y_plot_h, color='r')
-plt.plot(x_plot, hf_mean_lin_mf_model, '--', color='y')
-plt.plot(x_plot, hf_mean_high_gp_model, 'k--')
-plt.scatter(x_train_h, y_train_h, color='r')
-plt.xlabel('x')
-plt.ylabel('f (x)')
-plt.legend(['True Function', 'Linear Multi-fidelity GP', 'High fidelity GP'])
-plt.title('Comparison of linear multi-fidelity model and high fidelity GP')
-plt.savefig('multi_fidelity_GP.png')
+print(np.mean(np.abs(ctstar_statistical_model-training_data[:,3]))/0.75)
+print(np.max(np.abs(ctstar_statistical_model-training_data[:,3]))/0.75)
