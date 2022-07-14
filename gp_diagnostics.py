ff9093565833fe8994883f2c36aa55d8bf28916f
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2022-07-14 10:42:00 +0100

    Add GP diagnostics

---
+++ gp_diagnostics.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#load LES results
+LES_results = np.genfromtxt('training_data.csv', delimiter=',')
+
+#load statistical model results
+stat_model_mean = np.genfromtxt('ctstar_statistical_model.csv', delimiter=',')
+stat_model_std = np.genfromtxt('ctstar_statistical_model_std.csv', delimiter=',')
+
+standardised_errors = (stat_model_mean - LES_results[:,3])#/stat_model_std
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[8,6])
+ax[0,0].scatter(stat_model_mean, standardised_errors)
+#ax[0,0].set_ylim([-3.5,3.5])
+ax[0,0].axhline(y=0)
+ax[0,0].set_xlabel(r'$C_T^*$ prediction')
+
+ax[0,1].scatter(LES_results[:,0], standardised_errors)
+#ax[0,1].set_ylim([-3.5,3.5])
+ax[0,1].set_xlabel(r'$S_x$')
+ax[0,1].axhline(y=0)
+
+ax[1,0].scatter(LES_results[:,1], standardised_errors)
+#ax[1,0].set_ylim([-3.5,3.5])
+ax[1,0].set_xlabel(r'$S_y$')
+ax[1,0].axhline(y=0)
+
+ax[1,1].scatter(LES_results[:,2], standardised_errors)
+#ax[1,1].set_ylim([-3.5,3.5])
+ax[1,1].set_xlabel(r'$\theta$ ($^o$)')
+ax[1,1].axhline(y=0)
+
+plt.tight_layout()
+plt.savefig('gp_diagnostics.png')
