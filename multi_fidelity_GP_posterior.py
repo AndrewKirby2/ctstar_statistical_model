89bab51f768389286bec247dabf4b8a24d8e9cb2
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2022-07-26 13:10:56 +0100

    Plot posterior mean and variances

--- multi_fidelity_GP_posterior.py
+++ multi_fidelity_GP_posterior.py
@@ -23,7 +23,7 @@ ctstar_statistical_model_std = np.zeros(50)
 
 from emukit.multi_fidelity.convert_lists_to_array import convert_x_list_to_array, convert_xy_lists_to_arrays
 
-fit_model = True
+fit_model = False
 
 if fit_model:
 
@@ -61,3 +61,67 @@ else:
     #load gp model
     with open('model.pkl', 'rb') as file:
         lin_mf_model = pickle.load(file)
+    
+    print(lin_mf_model.gpy_model)
+    print(lin_mf_model.gpy_model.multifidelity.rbf.lengthscale)
+    print(lin_mf_model.gpy_model.multifidelity.rbf_1.lengthscale)
+
+#create test points
+n_x = 50
+n_y = 50
+x = np.linspace(500, 1000, n_x)
+y = np.linspace(500, 1000, n_y)
+xx, yy = np.meshgrid(x,y)
+
+#loop over different values for theta
+for z in np.arange(0,46,5):
+
+    X_test = np.array([xx.flatten(),yy.flatten(),z*np.ones(n_x*n_y)]).transpose()
+    scaler = StandardScaler()
+    scaler.fit(X_high)
+    X_test_stan = scaler.transform(X_test)
+    X_test_l = np.concatenate([np.atleast_2d(X_test_stan), np.zeros((X_test_stan.shape[0], 1))], axis=1)
+    X_test_h = np.concatenate([np.atleast_2d(X_test_stan), np.ones((X_test_stan.shape[0], 1))], axis=1)
+
+    lf_mean_mf_model, lf_var_mf_model = lin_mf_model.predict(X_test_l)
+    hf_mean_mf_model, hf_var_mf_model = lin_mf_model.predict(X_test_h)
+    hf_mean_mf_model = np.reshape(hf_mean_mf_model.flatten(),(n_x,n_y))
+    lf_mean_mf_model = np.reshape(lf_mean_mf_model.flatten(),(n_x,n_y))
+    lf_var_mf_model = np.reshape(lf_var_mf_model.flatten(),(n_x,n_y))
+    hf_var_mf_model = np.reshape(hf_var_mf_model.flatten(),(n_x,n_y))
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=[8,3])
+    pcm_lf = ax[0].pcolormesh(xx, yy, lf_mean_mf_model, vmin=0.5, vmax=0.8)
+    ax[0].set_xlabel(r'$S_x$ (m)')
+    ax[0].set_ylabel(r'$S_y$ (m)')
+    ax[0].set_title(r'a) $f_{low}(x)$', loc = 'left')
+    cbar = fig.colorbar(pcm_lf, ax=ax[0], shrink=0.97)
+    cbar.set_label(r'Posterior mean')
+
+    pcm_hf = ax[1].pcolormesh(xx, yy, hf_mean_mf_model-0.8298110784822755*lf_mean_mf_model, vmin=0.075, vmax = 0.15)
+    ax[1].set_xlabel(r'$S_x$ (m)')
+    ax[1].set_ylabel(r'$S_y$ (m)')
+    ax[1].set_title(r'b) $f_{err}(x)$', loc = 'left')
+    plt.tight_layout()
+    cbar = fig.colorbar(pcm_hf, ax=ax[1], shrink=0.97)
+    cbar.set_label(r'Posterior mean')
+    plt.savefig(f'mf_gp_posterior/posterior_mean_theta{z}.png')
+    plt.close()
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=[8,3])
+    pcm_lf = ax[0].pcolormesh(xx, yy, lf_var_mf_model, vmin=0, vmax=5e-6)
+    ax[0].set_xlabel(r'$S_x$ (m)')
+    ax[0].set_ylabel(r'$S_y$ (m)')
+    ax[0].set_title(r'a) $f_{low}(x)$', loc = 'left')
+    cbar = fig.colorbar(pcm_lf, ax=ax[0], shrink=0.97)
+    cbar.set_label(r'Posterior variance')
+
+    pcm_hf = ax[1].pcolormesh(xx, yy, hf_var_mf_model, vmin=0, vmax=2e-4)
+    ax[1].set_xlabel(r'$S_x$ (m)')
+    ax[1].set_ylabel(r'$S_y$ (m)')
+    ax[1].set_title(r'b) $f_{high}(x)$', loc = 'left')
+    plt.tight_layout()
+    cbar = fig.colorbar(pcm_hf, ax=ax[1], shrink=0.97)
+    cbar.set_label(r'Posterior variance')
+    plt.savefig(f'mf_gp_posterior/posterior_std_theta{z}.png')
+    plt.close()
