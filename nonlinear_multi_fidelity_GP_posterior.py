4e4a6ee61d9839561e3bdcde25f8d226d3c5e482
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2022-11-04 14:18:12 +0000

    Compare posterior mean functions

--- nonlinear_multi_fidelity_GP_posterior.py
+++ nonlinear_multi_fidelity_GP_posterior.py
@@ -146,3 +146,54 @@ cbar = fig.colorbar(pcm, ax=ax.ravel().tolist(), location='bottom')
 cbar.solids.set_rasterized(True)
 cbar.set_label(r'$\sqrt{\overline{k}_{\sigma^2}}$')
 plt.savefig('figures/MF-GP-nlow500_posterior_std_hf.png')
+
+#################################
+# 3. Plot posterior mean function
+# of g_high  and g_low for 
+# MF-GP-nlow500
+#################################
+
+#create plot
+cm = 1/2.54
+fig, ax = plt.subplots(nrows=2, ncols=5, figsize=[18.0*cm,10*cm], dpi=600)
+
+#loop over different theta values
+for i in range(5):
+    for j in range(2):
+        theta = i*10
+
+        #create test data and standardise
+        X_test = np.array([xx.flatten(),yy.flatten(),theta*np.ones(n_x*n_y)]).transpose()
+        X_test_stan = scaler.transform(X_test)
+        X_test_l = np.concatenate([np.atleast_2d(X_test_stan), np.zeros((X_test_stan.shape[0], 1))], axis=1)
+        X_test_h = np.concatenate([np.atleast_2d(X_test_stan), np.ones((X_test_stan.shape[0], 1))], axis=1)
+
+        #make predictions
+        lf_mean_mf_model, lf_var_mf_model = nonlin_mf_model.predict(X_test_l)
+        hf_mean_mf_model, hf_var_mf_model = nonlin_mf_model.predict(X_test_h)
+        hf_mean_mf_model = np.reshape(hf_mean_mf_model.flatten(),(n_x,n_y))
+        lf_mean_mf_model = np.reshape(lf_mean_mf_model.flatten(),(n_x,n_y))
+        lf_var_mf_model = np.reshape(lf_var_mf_model.flatten(),(n_x,n_y))
+        hf_var_mf_model = np.reshape(hf_var_mf_model.flatten(),(n_x,n_y))
+        lf_std_mf_model = np.sqrt(lf_var_mf_model)
+        hf_std_mf_model = np.sqrt(hf_var_mf_model)
+
+        if j==0:
+            if i+j==0:
+                pcm = ax[j, i].pcolormesh(xx/100, yy/100, hf_mean_mf_model, vmin=0.5, vmax=0.8, rasterized=True)
+            else:
+                ax[j, i].pcolormesh(xx/100, yy/100, hf_mean_mf_model, vmin=0.5, vmax=0.8, rasterized=True)
+            if i==0:
+                ax[j, i].set_ylabel(r'$S_y/D$')
+        if j==1:
+            ax[j, i].pcolormesh(xx/100, yy/100, lf_mean_mf_model, vmin=0.5, vmax=0.8, rasterized=True)
+            ax[j, i].set_xlabel(r'$S_x/D$')
+        ax[j, i].set_title(f'{string.ascii_lowercase[j*5+i]}) '+r'$\theta=$'+f'{i*10}'+r'$^o$', loc='left')
+        ax[j, i].set_aspect('equal')
+
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.3)
+cbar = fig.colorbar(pcm, ax=ax.ravel().tolist(), location='bottom')
+cbar.solids.set_rasterized(True)
+cbar.set_label(r'$\overline{m}_{\sigma^2}$')
+plt.savefig('figures/MF-GP-nlow500_posterior_mean_hf_lf.png')
